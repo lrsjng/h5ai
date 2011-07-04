3e6197d2fc25ce2059c10ab53ce704f9b5d8c2de
Lars Jung <lrsjng@gmail.com>
2011-07-04 12:51:34 +0200

    Added comments in options.js.

--- target/h5ai/options.js
+++ target/h5ai/options.js
@@ -1,7 +1,33 @@
 
 h5aiOptions = {
-	viewmodes: [ "icons", "details" ],
+
+	/*
+	 * An array of view modes the user may chose from. Currently there
+	 * are two possible values: "details" and "icons". The first value
+	 * indicates the default view mode. If only one value is given the
+	 * view mode is fixed and the selector buttons are hidden.
+	 * The user selected view mode is also stored local in modern browsers
+	 * so that it will be persistent.
+	 */
+	viewmodes: [ "details", "icons" ],
+
+	/*
+	 * Show a folder tree, boolean.
+	 * Note that this tree might have side effects as it sends HEAD requests
+	 * to the folders, and therefore will invoke index.php scripts. Use
+	 * folderStatus below to avoid such requests. 
+	 */
 	showTree: false,
+
+	/*
+	 * Associative array of folders and their HTTP status codes to
+	 * avoid HEAD requests to that folders.  
+	 */
 	folderStatus: {
+		/*
+		 * for example:
+		 * "/some/folder/": 200
+		 */
 	}
+
 };
