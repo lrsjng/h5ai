ca4d3ae315efac51961990311732f5f56c961f41
Lars Jung <lrsjng@gmail.com>
2012-02-17 01:18:57 +0100

    Adds classes to <html> to distinguish h5ai versions.

--- src/_h5ai/header.php
+++ src/_h5ai/header.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="h5ai-php no-js" lang="en"> <!--<![endif]-->
 <?php include "php/main.php"; ?>
 <head>
 	<meta charset="utf-8">
