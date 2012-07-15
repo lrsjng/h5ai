1828855f636c0cb2dd10b91d8fcac8b4dc69fb89
Lars Jung <lrsjng@gmail.com>
2012-07-15 19:34:20 +0200

    Single source of truth for current location.

--- src/_h5ai/js/inc/core/entry.js
+++ src/_h5ai/js/inc/core/entry.js
@@ -1,12 +1,11 @@
 
 modulejs.define('core/entry', ['$', 'core/parser', 'model/entry'], function ($, parser, Entry) {
 
-	var absHref = document.location.href.replace(/^.*:\/\/[^\/]*/, '').replace(/[^\/]*$/, '');
+	var entry = Entry.get();
 
-	parser.parse(absHref, $('body'));
+	parser.parse(entry.absHref, $('body'));
 	$('#data-apache-autoindex').remove();
 
-	var entry = Entry.get(absHref);
 	entry.status = 'h5ai';
 	if (entry.parent) {
 		entry.parent.isParentFolder = true;
