68497e749bf7ab18a621bb4813f792a3e465ae73
Lars Jung <lrsjng@gmail.com>
2012-07-16 17:23:01 +0200

    Hopefully fixed href bugs for all browsers now.

--- src/_h5ai/js/inc/model/entry.js
+++ src/_h5ai/js/inc/model/entry.js
@@ -2,7 +2,19 @@
 modulejs.define('model/entry', ['_', 'core/types', 'core/ajax'], function (_, types, ajax) {
 
 	var domain = document.domain,
-		location = document.location.href.replace(/^.*:\/\/[^\/]*/, '').replace(/[^\/]*$/, ''),
+		location = (function () {
+
+			var location = document.location.pathname.replace(/[^\/]*$/, ''),
+				testpathname = '/a b',
+				a = document.createElement('a');
+
+			a.href = testpathname;
+			if (a.pathname === testpathname) {
+				location = encodeURIComponent(location).replace(/%2F/ig, '/').replace(/'/g, '%27');
+			}
+
+			return location;
+		}()),
 
 
 		// utils
