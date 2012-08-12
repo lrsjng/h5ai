1be5e121b3496c1e1b41a70111a8be96b5151b7a
Lars Jung <lrsjng@gmail.com>
2012-08-12 14:56:05 +0200

    Cleans code.

--- src/_h5ai/js/inc/core/langs.js
+++ src/_h5ai/js/inc/core/langs.js
@@ -1,12 +1,5 @@
 
 modulejs.define('core/langs', ['config', '_'], function (config, _) {
 
-	var langs = {};
-
-	_.each(_.extend({}, config.langs), function (trans, lang) {
-
-		langs[lang] = trans;
-	});
-
-	return langs;
+	return _.extend({}, config.langs);
 });
