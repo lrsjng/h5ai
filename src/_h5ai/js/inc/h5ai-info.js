4b7aba378c4be09902718d1b3efb1ca415fa4d2c
Lars Jung <lrsjng@gmail.com>
2012-08-19 22:44:10 +0200

    Changes on info index page.

--- src/_h5ai/js/inc/h5ai-info.js
+++ src/_h5ai/js/inc/h5ai-info.js
@@ -3,12 +3,12 @@ modulejs.define('h5ai-info', ['$', 'core/ajax'], function ($, ajax) {
 
 	var setCheckResult = function (id, result) {
 
-			var $ele = $(id).find('.test-result');
+			var $result = $(id).find('.test-result');
 
 			if (result) {
-				$ele.addClass('test-passed').text('yes');
+				$result.addClass('test-passed').text('yes');
 			} else {
-				$ele.addClass('test-failed').text('no');
+				$result.addClass('test-failed').text('no');
 			}
 		},
 
