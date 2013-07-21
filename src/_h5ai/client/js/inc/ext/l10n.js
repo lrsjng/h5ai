ede9f4d3f8fd8ebaea8cce5a24b17766aa0dffa3
Lars Jung <lrsjng@gmail.com>
2013-07-21 22:23:16 +0200

    Fixes setParentFolderLabels=false.

--- src/_h5ai/client/js/inc/ext/l10n.js
+++ src/_h5ai/client/js/inc/ext/l10n.js
@@ -151,14 +151,11 @@ modulejs.define('ext/l10n', ['_', '$', 'core/settings', 'core/langs', 'core/form
 
 		init = function () {
 
-			if (!settings.enabled) {
-				event.sub('ready', function () { update(); });
-				return;
+			if (settings.enabled) {
+				initLangSelector(langs);
 			}
 
-			initLangSelector(langs);
-
-			event.sub('ready', function () {
+			event.sub('location.changed', function () {
 
 				localize(langs, settings.lang, settings.useBrowserLang);
 			});
