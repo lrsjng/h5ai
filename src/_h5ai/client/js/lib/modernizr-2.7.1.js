99e2f732a05f0dac0ace25643bc3f091f9a1c815
XhmikosR <xhmikosr@users.sourceforge.net>
2013-12-03 11:03:03 +0200

    Update modernizr to v2.7.1.

--- src/_h5ai/client/js/lib/modernizr-2.7.1.js
+++ src/_h5ai/client/js/lib/modernizr-2.7.1.js
@@ -1,4 +1,4 @@
-/* Modernizr 2.6.2 (Custom Build) | MIT & BSD
+/* Modernizr 2.7.1 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-opacity-rgba-canvas-history-audio-video-localstorage-shiv-cssclasses-prefixes
  */
 ;
@@ -7,7 +7,7 @@
 
 window.Modernizr = (function( window, document, undefined ) {
 
-    var version = '2.6.2',
+    var version = '2.7.1',
 
     Modernizr = {},
 
@@ -239,24 +239,26 @@ window.Modernizr = (function( window, document, undefined ) {
     modElem = inputElem = null;
 
     ;(function(window, document) {
-        var options = window.html5 || {};
+                var version = '3.7.0';
 
-        var reSkip = /^<|^(?:button|map|select|textarea|object|iframe|option|optgroup)$/i;
+            var options = window.html5 || {};
 
-        var saveClones = /^(?:a|b|code|div|fieldset|h1|h2|h3|h4|h5|h6|i|label|li|ol|p|q|span|strong|style|table|tbody|td|th|tr|ul)$/i;
+            var reSkip = /^<|^(?:button|map|select|textarea|object|iframe|option|optgroup)$/i;
 
-        var supportsHtml5Styles;
+            var saveClones = /^(?:a|b|code|div|fieldset|h1|h2|h3|h4|h5|h6|i|label|li|ol|p|q|span|strong|style|table|tbody|td|th|tr|ul)$/i;
 
-        var expando = '_html5shiv';
+            var supportsHtml5Styles;
 
-        var expanID = 0;
+            var expando = '_html5shiv';
 
-        var expandoData = {};
+            var expanID = 0;
 
-        var supportsUnknownElements;
+            var expandoData = {};
 
-      (function() {
-        try {
+            var supportsUnknownElements;
+
+        (function() {
+          try {
             var a = document.createElement('a');
             a.innerHTML = '<xyz></xyz>';
                     supportsHtml5Styles = ('hidden' in a);
@@ -270,138 +272,149 @@ window.Modernizr = (function( window, document, undefined ) {
                 typeof frag.createElement == 'undefined'
               );
             }());
-        } catch(e) {
-          supportsHtml5Styles = true;
-          supportsUnknownElements = true;
+          } catch(e) {
+                    supportsHtml5Styles = true;
+            supportsUnknownElements = true;
+          }
+
+        }());
+
+            function addStyleSheet(ownerDocument, cssText) {
+          var p = ownerDocument.createElement('p'),
+          parent = ownerDocument.getElementsByTagName('head')[0] || ownerDocument.documentElement;
+
+          p.innerHTML = 'x<style>' + cssText + '</style>';
+          return parent.insertBefore(p.lastChild, parent.firstChild);
         }
 
-      }());        function addStyleSheet(ownerDocument, cssText) {
-        var p = ownerDocument.createElement('p'),
-            parent = ownerDocument.getElementsByTagName('head')[0] || ownerDocument.documentElement;
+            function getElements() {
+          var elements = html5.elements;
+          return typeof elements == 'string' ? elements.split(' ') : elements;
+        }
 
-        p.innerHTML = 'x<style>' + cssText + '</style>';
-        return parent.insertBefore(p.lastChild, parent.firstChild);
-      }
-
-        function getElements() {
-        var elements = html5.elements;
-        return typeof elements == 'string' ? elements.split(' ') : elements;
-      }
-
-          function getExpandoData(ownerDocument) {
-        var data = expandoData[ownerDocument[expando]];
-        if (!data) {
+            function getExpandoData(ownerDocument) {
+          var data = expandoData[ownerDocument[expando]];
+          if (!data) {
             data = {};
             expanID++;
             ownerDocument[expando] = expanID;
             expandoData[expanID] = data;
+          }
+          return data;
         }
-        return data;
-      }
 
-        function createElement(nodeName, ownerDocument, data){
-        if (!ownerDocument) {
+            function createElement(nodeName, ownerDocument, data){
+          if (!ownerDocument) {
             ownerDocument = document;
-        }
-        if(supportsUnknownElements){
+          }
+          if(supportsUnknownElements){
             return ownerDocument.createElement(nodeName);
-        }
-        if (!data) {
+          }
+          if (!data) {
             data = getExpandoData(ownerDocument);
-        }
-        var node;
+          }
+          var node;
 
-        if (data.cache[nodeName]) {
+          if (data.cache[nodeName]) {
             node = data.cache[nodeName].cloneNode();
-        } else if (saveClones.test(nodeName)) {
+          } else if (saveClones.test(nodeName)) {
             node = (data.cache[nodeName] = data.createElem(nodeName)).cloneNode();
-        } else {
+          } else {
             node = data.createElem(nodeName);
+          }
+
+                                                    return node.canHaveChildren && !reSkip.test(nodeName) && !node.tagUrn ? data.frag.appendChild(node) : node;
         }
 
-                                    return node.canHaveChildren && !reSkip.test(nodeName) ? data.frag.appendChild(node) : node;
-      }
-
-        function createDocumentFragment(ownerDocument, data){
-        if (!ownerDocument) {
+            function createDocumentFragment(ownerDocument, data){
+          if (!ownerDocument) {
             ownerDocument = document;
-        }
-        if(supportsUnknownElements){
+          }
+          if(supportsUnknownElements){
             return ownerDocument.createDocumentFragment();
-        }
-        data = data || getExpandoData(ownerDocument);
-        var clone = data.frag.cloneNode(),
-            i = 0,
-            elems = getElements(),
-            l = elems.length;
-        for(;i<l;i++){
+          }
+          data = data || getExpandoData(ownerDocument);
+          var clone = data.frag.cloneNode(),
+          i = 0,
+          elems = getElements(),
+          l = elems.length;
+          for(;i<l;i++){
             clone.createElement(elems[i]);
+          }
+          return clone;
         }
-        return clone;
-      }
 
-        function shivMethods(ownerDocument, data) {
-        if (!data.cache) {
+            function shivMethods(ownerDocument, data) {
+          if (!data.cache) {
             data.cache = {};
             data.createElem = ownerDocument.createElement;
             data.createFrag = ownerDocument.createDocumentFragment;
             data.frag = data.createFrag();
+          }
+
+
+          ownerDocument.createElement = function(nodeName) {
+                    if (!html5.shivMethods) {
+              return data.createElem(nodeName);
+            }
+            return createElement(nodeName, ownerDocument, data);
+          };
+
+          ownerDocument.createDocumentFragment = Function('h,f', 'return function(){' +
+                                                          'var n=f.cloneNode(),c=n.createElement;' +
+                                                          'h.shivMethods&&(' +
+                                                                                                                getElements().join().replace(/[\w\-]+/g, function(nodeName) {
+            data.createElem(nodeName);
+            data.frag.createElement(nodeName);
+            return 'c("' + nodeName + '")';
+          }) +
+            ');return n}'
+                                                         )(html5, data.frag);
         }
 
-
-        ownerDocument.createElement = function(nodeName) {
-                if (!html5.shivMethods) {
-              return data.createElem(nodeName);
+            function shivDocument(ownerDocument) {
+          if (!ownerDocument) {
+            ownerDocument = document;
           }
-          return createElement(nodeName, ownerDocument, data);
+          var data = getExpandoData(ownerDocument);
+
+          if (html5.shivCSS && !supportsHtml5Styles && !data.hasCSS) {
+            data.hasCSS = !!addStyleSheet(ownerDocument,
+                                                                                'article,aside,dialog,figcaption,figure,footer,header,hgroup,main,nav,section{display:block}' +
+                                                                                    'mark{background:#FF0;color:#000}' +
+                                                                                    'template{display:none}'
+                                         );
+          }
+          if (!supportsUnknownElements) {
+            shivMethods(ownerDocument, data);
+          }
+          return ownerDocument;
+        }
+
+            var html5 = {
+
+                'elements': options.elements || 'abbr article aside audio bdi canvas data datalist details dialog figcaption figure footer header hgroup main mark meter nav output progress section summary template time video',
+
+                'version': version,
+
+                'shivCSS': (options.shivCSS !== false),
+
+                'supportsUnknownElements': supportsUnknownElements,
+
+                'shivMethods': (options.shivMethods !== false),
+
+                'type': 'default',
+
+                'shivDocument': shivDocument,
+
+                createElement: createElement,
+
+                createDocumentFragment: createDocumentFragment
         };
 
-        ownerDocument.createDocumentFragment = Function('h,f', 'return function(){' +
-          'var n=f.cloneNode(),c=n.createElement;' +
-          'h.shivMethods&&(' +
-                    getElements().join().replace(/\w+/g, function(nodeName) {
-              data.createElem(nodeName);
-              data.frag.createElement(nodeName);
-              return 'c("' + nodeName + '")';
-            }) +
-          ');return n}'
-        )(html5, data.frag);
-      }        function shivDocument(ownerDocument) {
-        if (!ownerDocument) {
-            ownerDocument = document;
-        }
-        var data = getExpandoData(ownerDocument);
+            window.html5 = html5;
 
-        if (html5.shivCSS && !supportsHtml5Styles && !data.hasCSS) {
-          data.hasCSS = !!addStyleSheet(ownerDocument,
-                    'article,aside,figcaption,figure,footer,header,hgroup,nav,section{display:block}' +
-                    'mark{background:#FF0;color:#000}'
-          );
-        }
-        if (!supportsUnknownElements) {
-          shivMethods(ownerDocument, data);
-        }
-        return ownerDocument;
-      }        var html5 = {
-
-            'elements': options.elements || 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video',
-
-            'shivCSS': (options.shivCSS !== false),
-
-            'supportsUnknownElements': supportsUnknownElements,
-
-            'shivMethods': (options.shivMethods !== false),
-
-            'type': 'default',
-
-            'shivDocument': shivDocument,
-
-            createElement: createElement,
-
-            createDocumentFragment: createDocumentFragment
-      };        window.html5 = html5;
-
-        shivDocument(document);
+            shivDocument(document);
 
     }(this, document));
 
