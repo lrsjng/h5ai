c224de1994beaeb3d48698f6f0f1671d9e4768af
Lars Jung <lrsjng@gmail.com>
2014-06-01 20:37:29 +0200

    More URL fixes.

--- src/_h5ai/client/js/inc/core/location.js
+++ src/_h5ai/client/js/inc/core/location.js
@@ -14,14 +14,25 @@ modulejs.define('core/location', ['_', 'modernizr', 'core/settings', 'core/event
 
 			return href
 					.replace(/\/+/g, '/')
-					.replace(/ /g, '%20')
+
+					.replace(/!/g, '%21')
+					.replace(/#/g, '%23')
+					.replace(/\$/g, '%24')
+					.replace(/&/g, '%26')
 					.replace(/'/g, '%27')
-					.replace(/\[/g, '%5B')
-					.replace(/\]/g, '%5D')
 					.replace(/\(/g, '%28')
 					.replace(/\)/g, '%29')
+					.replace(/\*/g, '%2A')
 					.replace(/\+/g, '%2B')
-					.replace(/\=/g, '%3D');
+					.replace(/\,/g, '%2C')
+					// .replace(/\//g, '%2F')
+					.replace(/:/g, '%3A')
+					.replace(/;/g, '%3B')
+					.replace(/=/g, '%3D')
+					.replace(/\?/g, '%3F')
+					.replace(/@/g, '%40')
+					.replace(/\[/g, '%5B')
+					.replace(/\]/g, '%5D');
 		},
 
 		reUriToPathname = /^.*:\/\/[^\/]*|[^\/]*$/g,
