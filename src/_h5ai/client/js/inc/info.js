ca816a57455a5727c433501f7e80b70a22e02c4e
Lars Jung <lrsjng@gmail.com>
2014-06-13 21:39:28 +0200

    Update info page.

--- src/_h5ai/client/js/inc/info.js
+++ src/_h5ai/client/js/inc/info.js
@@ -1,44 +1,90 @@
 
 modulejs.define('info', ['$', 'config'], function ($, config) {
 
-	var map = function (setup) {
+	var template = '<li class="test">' +
+				'<span class="label"></span>' +
+				'<span class="result"></span>' +
+				'<div class="info"></div>' +
+			'</li>',
 
-			return {
-				'php_version': setup.HAS_PHP_VERSION,
-				'cache_dir': setup.HAS_WRITABLE_CACHE,
-				'image_thumbs': setup.HAS_PHP_JPG,
-				'exif_thumbs': setup.HAS_PHP_EXIF,
-				'movie_thumbs': setup.HAS_CMD_FFMPEG || setup.HAS_CMD_AVCONV,
-				'pdf_thumbs': setup.HAS_CMD_CONVERT,
-				'shell_tar': setup.HAS_CMD_TAR,
-				'shell_zip': setup.HAS_CMD_ZIP,
-				'folder_sizes': setup.HAS_CMD_DU
-			};
-		},
+		$tests = $('#tests'),
 
-		setValue = function (el, result) {
+		addTestResult = function (label, info, passed, result) {
 
-			var $result = $(el).find('.result');
-
-			if (result) {
-				$result.addClass('passed').text('yes');
-			} else {
-				$result.addClass('failed').text('no');
-			}
+			$(template)
+				.find('.label')
+					.text(label)
+				.end()
+				.find('.result')
+					.addClass(passed ? 'passed' : 'failed')
+					.text(result ? result : (passed ? 'yes' : 'no'))
+				.end()
+				.find('.info')
+					.html(info)
+				.end()
+				.appendTo($tests);
 		},
 
 		init = function () {
 
-			var setup = config.setup,
-				values = map(setup);
-
-			$('.test').each(function () {
-
-				setValue(this, values[$(this).data('id')]);
-			});
+			var setup = config.setup;
 
 			$('.idx-file .value').text(setup.INDEX_HREF);
-			$('.test.php .result').text(setup.PHP_VERSION);
+
+			addTestResult(
+				'PHP version',
+				'PHP version &gt;= 5.3.0',
+				setup.HAS_PHP_VERSION,
+				setup.PHP_VERSION
+			);
+
+			addTestResult(
+				'Cache directory',
+				'Web server has write access',
+				setup.HAS_WRITABLE_CACHE
+			);
+
+			addTestResult(
+				'Image thumbs',
+				'PHP GD extension with JPEG support available',
+				setup.HAS_PHP_JPG
+			);
+
+			addTestResult(
+				'Use EXIF thumbs',
+				'PHP EXIF extension available',
+				setup.HAS_PHP_EXIF
+			);
+
+			addTestResult(
+				'Movie thumbs',
+				'Command line program <code>ffmpeg</code> or <code>avconv</code> available',
+				setup.HAS_CMD_FFMPEG || setup.HAS_CMD_AVCONV
+			);
+
+			addTestResult(
+				'PDF thumbs',
+				'Command line program <code>convert</code> available',
+				setup.HAS_CMD_CONVERT
+			);
+
+			addTestResult(
+				'Shell tar',
+				'Command line program <code>tar</code> available',
+				setup.HAS_CMD_TAR
+			);
+
+			addTestResult(
+				'Shell zip',
+				'Command line program <code>zip</code> available',
+				setup.HAS_CMD_ZIP
+			);
+
+			addTestResult(
+				'Folder sizes',
+				'Command line program <code>du</code> available',
+				setup.HAS_CMD_DU
+			);
 		};
 
 	init();
