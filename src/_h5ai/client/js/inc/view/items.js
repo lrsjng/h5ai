4df73eb45c7f6cb6f350ef3f18f6f29cbfdc274d
Lars Jung <lrsjng@gmail.com>
2014-11-21 01:56:49 +0100

    Add hint to item labels.

--- src/_h5ai/client/js/inc/view/items.js
+++ src/_h5ai/client/js/inc/view/items.js
@@ -54,6 +54,7 @@ modulejs.define('view/items', ['_', '$', 'core/settings', 'core/resource', 'core
 
         $iconImg.attr('src', resource.icon(item.type)).attr('alt', item.type);
         $label.text(item.label);
+        $label.attr('title', item.label);
         $date.data('time', item.time).text(format.formatDate(item.time));
         $size.data('bytes', item.size).text(format.formatSize(item.size));
 
