dea5092ae05e9cb1374dd8212036a06fd6f98ca0
Lars Jung <lrsjng@gmail.com>
2015-03-14 15:51:39 +0100

    Clean code.

--- src/_h5ai/client/js/inc/view/ensure.js
+++ src/_h5ai/client/js/inc/view/ensure.js
@@ -18,46 +18,11 @@ modulejs.define('view/ensure', ['$', 'config', 'core/event'], function ($, confi
             '</a>';
 
 
-    var selb = '#bottombar';
-    var sela = selb + ' > a';
-    var sequence = 'powered by h5ai ' + config.setup.VERSION;
-    var url = 'http://larsjung.de/h5ai/';
-    var isVisible = ':visible';
-    var styleKey = 'style';
-
-
-    function ensure() {
-
-        if (
-            $(sela).text() !== sequence ||
-            $(sela).attr('href') !== url ||
-            $(sela).filter(isVisible).length !== 1 ||
-            $(selb).filter(isVisible).length !== 1
-        ) {
-            $(selb).remove();
-            $('<div id="bottombar"/>')
-                .attr(styleKey, 'display: block !important')
-                .appendTo('body');
-            $('<a/>')
-                .attr(styleKey, 'display: inline !important')
-                .attr('href', url)
-                .attr('title', sequence)
-                .text(sequence)
-                .appendTo(selb);
-        }
-    }
-
     function init() {
 
         $(templateTopbar).appendTo('body');
         $(templateMainRow).appendTo('body');
         $(templateBacklink).appendTo('#topbar');
-
-        // event.sub('ready', function () {
-
-        //     ensure();
-        //     setInterval(ensure, 60000);
-        // });
     }
 
 
