668f316465495067bf19958d402b9d0d241ae839
Lars Jung <lrsjng@gmail.com>
2015-04-26 18:10:31 +0200

    Refactor tests.

--- test/scripts.js
+++ test/scripts.js
@@ -19,17 +19,23 @@ util.setupMocha();
 
 describe('all tests', function () {
 
+    // @include "tests/premisses.js"
+
     describe('unit tests', function () {
-        // @include "tests/unit/premisses.js"
+
         // @include "tests/unit/modulejs.js"
         // @include "tests/unit/libs.js"
         // @include "tests/unit/boot.js"
         // @include "tests/unit/config.js"
+
         // @include "tests/unit/*/*.js"
     });
 
     describe('integration tests', function () {
-        // @include "tests/integration/**/*.js"
+
+        // @include "tests/integration/first.js"
+
+        // @include "tests/integration/*/*.js"
     });
 });
 
