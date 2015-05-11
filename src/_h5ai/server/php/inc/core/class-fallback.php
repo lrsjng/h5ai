d07850b6ef27061c807783cb3d596b1c51a92643
Lars Jung <lrsjng@gmail.com>
2015-05-11 13:00:10 +0200

    Refactor PHP. Switch to explicit string literals where possible.

--- src/_h5ai/server/php/inc/core/class-fallback.php
+++ src/_h5ai/server/php/inc/core/class-fallback.php
@@ -33,36 +33,36 @@ class Fallback {
         $items = $folder->get_content($cache);
         uasort($items, ['Item', 'cmp']);
 
-        $html = "<table>";
+        $html = '<table>';
 
-        $html .= "<tr>";
-        $html .= "<th class='fb-i'></th>";
-        $html .= "<th class='fb-n'><span>Name</span></th>";
-        $html .= "<th class='fb-d'><span>Last modified</span></th>";
-        $html .= "<th class='fb-s'><span>Size</span></th>";
-        $html .= "</tr>";
+        $html .= '<tr>';
+        $html .= '<th class="fb-i"></th>';
+        $html .= '<th class="fb-n"><span>Name</span></th>';
+        $html .= '<th class="fb-d"><span>Last modified</span></th>';
+        $html .= '<th class="fb-s"><span>Size</span></th>';
+        $html .= '</tr>';
 
         if ($folder->get_parent($cache)) {
-            $html .= "<tr>";
-            $html .= "<td class='fb-i'><img src='${app_href}client/images/fallback/folder-parent.png' alt='folder-parent'/></td>";
-            $html .= "<td class='fb-n'><a href='..'>Parent Directory</a></td>";
-            $html .= "<td class='fb-d'></td>";
-            $html .= "<td class='fb-s'></td>";
-            $html .= "</tr>";
+            $html .= '<tr>';
+            $html .= '<td class="fb-i"><img src="' . $app_href . 'client/images/fallback/folder-parent.png" alt="folder-parent"/></td>';
+            $html .= '<td class="fb-n"><a href="..">Parent Directory</a></td>';
+            $html .= '<td class="fb-d"></td>';
+            $html .= '<td class="fb-s"></td>';
+            $html .= '</tr>';
         }
 
         foreach ($items as $item) {
             $type = $item->is_folder ? 'folder' : 'file';
 
-            $html .= "<tr>";
-            $html .= "<td class='fb-i'><img src='${app_href}client/images/fallback/${type}.png' alt='${type}'/></td>";
-            $html .= "<td class='fb-n'><a href='" . $item->href . "'>" . basename($item->path) . "</a></td>";
-            $html .= "<td class='fb-d'>" . date('Y-m-d H:i', $item->date) . "</td>";
-            $html .= "<td class='fb-s'>" . ($item->size !== null ? intval($item->size / 1000) . ' KB' : '' ) . "</td>";
-            $html .= "</tr>";
+            $html .= '<tr>';
+            $html .= '<td class="fb-i"><img src="' . $app_href . 'client/images/fallback/' . $type . '.png" alt="' . $type . '"/></td>';
+            $html .= '<td class="fb-n"><a href="' . $item->href . '">' . basename($item->path) . '</a></td>';
+            $html .= '<td class="fb-d">' . date('Y-m-d H:i', $item->date) . '</td>';
+            $html .= '<td class="fb-s">' . ($item->size !== null ? intval($item->size / 1000) . ' KB' : '' ) . '</td>';
+            $html .= '</tr>';
         }
 
-        $html .= "</table>";
+        $html .= '</table>';
 
         return $html;
     }
