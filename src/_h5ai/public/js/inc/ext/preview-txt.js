09fa3acc4de375222e524455686636be33be584a
Lars Jung <lrsjng@gmail.com>
2015-05-17 00:16:31 +0200

    Update text preview.

--- src/_h5ai/public/js/inc/ext/preview-txt.js
+++ src/_h5ai/public/js/inc/ext/preview-txt.js
@@ -6,90 +6,108 @@ modulejs.define('ext/preview-txt', ['_', '$', 'marked', 'prism', 'core/event', '
         }, allsettings['preview-txt']);
     var templateText = '<pre id="pv-txt-text" class="highlighted"/>';
     var templateMarkdown = '<div id="pv-txt-text" class="markdown"/>';
+    var spinnerThreshold = 200;
+    var spinnerTimeoutId;
+    var currentItems;
+    var currentIdx;
+    var currentItem;
 
 
-    function preloadText(absHref, callback) {
+    function preloadText(item, callback) {
 
         $.ajax({
-                url: absHref,
+                url: item.absHref,
                 dataType: 'text'
             })
             .done(function (content) {
 
-                callback(content);
-                // setTimeout(function () { callback(content); }, 1000); // for testing
+                // callback(item, content);
+
+                // for testing
+                setTimeout(function () { callback(item, content); }, 1000);
             })
             .fail(function (jqXHR, textStatus) {
 
-                callback('[ajax error] ' + textStatus);
+                callback(item, '[ajax error] ' + textStatus);
             });
+    }
+
+    function onAdjustSize() {
+
+        var $content = $('#pv-content');
+        var $text = $('#pv-txt-text');
+
+        if ($text.length) {
+            $text.height($content.height() - 16);
+        }
+    }
+
+    function onIdxChange(rel) {
+
+        currentIdx = (currentIdx + rel + currentItems.length) % currentItems.length;
+        currentItem = currentItems[currentIdx];
+
+        preview.setLabels([
+            currentItem.label,
+            String(currentItem.size) + ' bytes'
+        ]);
+        preview.setIndex(currentIdx + 1, currentItems.length);
+        preview.setRawLink(currentItem.absHref);
+
+        $('#pv-content').hide();
+        if (preview.isSpinnerVisible()) {
+            preview.showSpinner(true, currentItem.icon);
+        } else {
+            clearTimeout(spinnerTimeoutId);
+            spinnerTimeoutId = setTimeout(function () {
+
+                preview.showSpinner(true, currentItem.icon);
+            }, spinnerThreshold);
+        }
+
+        preloadText(currentItem, function (item, textContent) {
+
+            if (item !== currentItem) {
+                return;
+            }
+
+            var type = settings.types[currentItem.type];
+            var $text;
+            var $code;
+
+            if (type === 'none') {
+                $text = $(templateMarkdown).text(textContent);
+            } else if (type === 'fixed') {
+                $text = $(templateText).text(textContent);
+            } else if (type === 'markdown') {
+                $text = $(templateMarkdown).html(marked(textContent));
+            } else {
+                $text = $(templateText);
+                $code = $('<code/>').appendTo($text);
+
+                if (textContent.length < 20000) {
+                    $code.empty().html(prism.highlight(textContent, prism.languages[type]));
+                } else {
+                    $code.empty().text(textContent);
+                    setTimeout(function () { $code.empty().html(prism.highlight(textContent, prism.languages[type])); }, 300);
+                }
+            }
+
+            clearTimeout(spinnerTimeoutId);
+            preview.showSpinner(false);
+            $('#pv-content')
+                .empty()
+                .append($text)
+                .show();
+            onAdjustSize();
+        });
     }
 
     function onEnter(items, idx) {
 
-        var currentItems = items;
-        var currentIdx = idx;
-        var currentItem = items[idx];
-
-        function onAdjustSize() {
-
-            var $content = $('#pv-content');
-            var $text = $('#pv-txt-text');
-
-            if ($text.length) {
-
-                $text.height($content.height() - 16);
-            }
-        }
-
-        function onIdxChange(rel) {
-
-            currentIdx = (currentIdx + rel + currentItems.length) % currentItems.length;
-            currentItem = currentItems[currentIdx];
-
-            var spinnerTimeout = setTimeout(function () { preview.showSpinner(true); }, 200);
-
-            preloadText(currentItem.absHref, function (textContent) {
-
-                clearTimeout(spinnerTimeout);
-                preview.showSpinner(false);
-
-                $('#pv-content').fadeOut(100, function () {
-
-                    var type = settings.types[currentItem.type];
-                    var $text;
-                    var $code;
-
-                    if (type === 'none') {
-                        $text = $(templateMarkdown).text(textContent);
-                    } else if (type === 'fixed') {
-                        $text = $(templateText).text(textContent);
-                    } else if (type === 'markdown') {
-                        $text = $(templateMarkdown).html(marked(textContent));
-                    } else {
-                        $text = $(templateText);
-                        $code = $('<code/>').appendTo($text);
-
-                        if (textContent.length < 20000) {
-                            $code.empty().html(prism.highlight(textContent, prism.languages[type]));
-                        } else {
-                            $code.empty().text(textContent);
-                            setTimeout(function () { $code.empty().html(prism.highlight(textContent, prism.languages[type])); }, 300);
-                        }
-                    }
-                    $('#pv-content').empty().append($text).fadeIn(200);
-                    onAdjustSize();
-
-                    preview.setIndex(currentIdx + 1, currentItems.length);
-                    preview.setLabels([
-                        currentItem.label,
-                        String(currentItem.size) + ' bytes'
-                    ]);
-                    preview.setRawLink(currentItem.absHref);
-                });
-            });
-        }
-
+        currentItems = items;
+        currentIdx = idx;
+        currentItem = items[idx];
         onIdxChange(0);
         preview.setOnIndexChange(onIdxChange);
         preview.setOnAdjustSize(onAdjustSize);
