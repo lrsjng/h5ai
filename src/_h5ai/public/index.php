793bfce2022da817e0e478bf07b483253bc50fff
Lars Jung <lrsjng@gmail.com>
2016-05-27 22:42:11 +0200

    Minor change.

--- src/_h5ai/public/index.php
+++ src/_h5ai/public/index.php
@@ -5,8 +5,7 @@ define('MIN_PHP_VERSION', '5.5.0');
 
 if (!function_exists('version_compare') || version_compare(PHP_VERSION, MIN_PHP_VERSION, '<')) {
     header('Content-type: text/plain;charset=utf-8');
-    echo '[err] h5ai requires PHP ' . MIN_PHP_VERSION . ' or later, but found PHP ' . PHP_VERSION;
-    exit;
+    exit('[ERR] h5ai requires PHP ' . MIN_PHP_VERSION . ' or later, but found PHP ' . PHP_VERSION);
 }
 
 require_once __DIR__ . '/../private/php/class-bootstrap.php';
