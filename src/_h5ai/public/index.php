029d4ed5af3001a85391c000b86ca711ec588406
Lars Jung <lrsjng@gmail.com>
2016-06-22 11:36:16 +0200

    Minor.

--- src/_h5ai/public/index.php
+++ src/_h5ai/public/index.php
@@ -1,4 +1,4 @@
-<?php
+s<?php
 
 define('H5AI_VERSION', '{{VERSION}}');
 define('MIN_PHP_VERSION', '5.5.0');
@@ -8,7 +8,7 @@ if (!function_exists('version_compare') || version_compare(PHP_VERSION, MIN_PHP_
     exit('[ERR] h5ai requires PHP ' . MIN_PHP_VERSION . ' or later, but found PHP ' . PHP_VERSION);
 }
 
-if (substr(H5AI_VERSION, 0, 2) === '{{') {
+if (substr(H5AI_VERSION, 0, 1) === '{') {
     header('Content-type: text/plain;charset=utf-8');
     exit('[ERR] h5ai sources must be preprocessed to work correctly');
 }
