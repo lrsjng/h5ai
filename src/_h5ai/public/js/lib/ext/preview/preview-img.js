4a3dd108bd20cb598958c5563c2098629ffd9e03
glubsy <glubsy@users.noreply.github.com>
2021-02-06 04:10:00 +0000

    Remove client-side sample request for img
    * Down-sampling should only be requested by the backend.
    * For now remove the client-side logic entirely since it is both
    superfluous and vulnerable. This can be implemented again on the
    back-end if necessary.

--- src/_h5ai/public/js/lib/ext/preview/preview-img.js
+++ src/_h5ai/public/js/lib/ext/preview/preview-img.js
@@ -1,11 +1,9 @@
 const {dom} = require('../../util');
-const server = require('../../server');
 const allsettings = require('../../core/settings');
 const preview = require('./preview');
 
 const settings = Object.assign({
     enabled: false,
-    size: null,
     types: []
 }, allsettings['preview-img']);
 const tpl = '<img id="pv-content-img"/>';
@@ -19,34 +17,16 @@ const updateGui = () => {
     const elW = el.offsetWidth;
 
     const labels = [preview.item.label];
-    if (!settings.size) {
-        const elNW = el.naturalWidth;
-        const elNH = el.naturalHeight;
-        labels.push(String(elNW) + 'x' + String(elNH));
-        labels.push(String((100 * elW / elNW).toFixed(0)) + '%');
-    }
-    preview.setLabels(labels);
-};
+    const elNW = el.naturalWidth;
+    const elNH = el.naturalHeight;
+    labels.push(String(elNW) + 'x' + String(elNH));
+    labels.push(String((100 * elW / elNW).toFixed(0)) + '%');
 
-const requestSample = href => {
-    return server.request({
-        action: 'get',
-        thumbs: [{
-            type: 'img',
-            href,
-            width: settings.size,
-            height: 0
-        }]
-    }).then(json => {
-        return json && json.thumbs && json.thumbs[0] ? json.thumbs[0] : null;
-    });
+    preview.setLabels(labels);
 };
 
 const load = item => {
     return Promise.resolve(item.absHref)
-        .then(href => {
-            return settings.size ? requestSample(href) : href;
-        })
         .then(href => new Promise(resolve => {
             const $el = dom(tpl)
                 .on('load', () => resolve($el))
